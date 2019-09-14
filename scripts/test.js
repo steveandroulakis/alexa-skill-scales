3798fc0fa1c1e8a4b5eb922e27998cad39ce2c6a
Steve Androulakis <steve.androulakis@gmail.com>
2019-09-14 10:29:37 +1000

    request and response scaffolding for scales

---
+++ scripts/test.js
@@ -0,0 +1,91 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// https://medium.com/@equisept/simplest-typescript-with-visual-studio-code-e42843fe437
+const scale_library_json_1 = __importDefault(require("./scale_library.json"));
+// if attribute exists in the library (e.g. don't ask for 4 octaves)
+function validAttribute(attribute, type) {
+    return attribute in scale_library_json_1.default["scale"][type];
+}
+// based on defaults, what's saved for the user and what they requested
+function computeScaleAttributes(scaleAttributeSet) {
+    const scaleAttributes = {
+        key: undefined,
+        mode: undefined,
+        speed: undefined,
+        octave: undefined
+    };
+    // tslint:disable-next-line:forin
+    for (const key in scaleAttributeSet.defaultScaleAttributes) {
+        scaleAttributes[key] =
+            scaleAttributeSet.requestScaleAttributes[key] ||
+                scaleAttributeSet.savedScaleAttributes[key] ||
+                scaleAttributeSet.defaultScaleAttributes[key];
+    }
+    return scaleAttributes;
+}
+// can we fulfill the request? did the user request something invalid?
+function remainingScaleAttributes(computedAttributes) {
+    const rmAttrs = [];
+    // tslint:disable-next-line:forin
+    for (const key in computedAttributes) {
+        if (!validAttribute(computedAttributes[key], key)) {
+            rmAttrs.push({ key: key, input: computedAttributes[key] });
+        }
+    }
+    return rmAttrs;
+}
+// get a human-readable name like 'C sharp' for a request
+function humanName(attribute, type) {
+    return scale_library_json_1.default["scale"][type][attribute];
+}
+// respond with intro speech and a scale
+function speechScaleResponse(computedScale, requestedScale) {
+    const scaleSpeech = `Playing ${humanName(computedScale.key, "key")} ${humanName(computedScale.mode, "mode")}`;
+    let modifierSpeech = "";
+    if (requestedScale.octave) {
+        modifierSpeech = `${modifierSpeech}, ${humanName(computedScale.octave, "octave")}`;
+    }
+    if (requestedScale.speed) {
+        modifierSpeech = `${modifierSpeech}, ${humanName(computedScale.speed, "speed")}`;
+    }
+    return scaleSpeech + modifierSpeech + ".";
+}
+function computeScaleFilename(computedScale) {
+    const tempos = { slow: "80", normal: "120", fast: "160" };
+    // scale_E_natural_minor_1o_120
+    const scaleFilename = `scale_${computedScale.key}_${computedScale.mode}_${computedScale.octave}o_${tempos[computedScale.speed]}.mp3`;
+    return scaleFilename;
+}
+/// -----------------------------------------------------------------------------
+const defaultScaleAttributes = {
+    key: undefined,
+    mode: "major",
+    speed: "normal",
+    octave: "1"
+};
+const savedScaleAttributes = {
+    key: "D",
+    mode: "natural_minor",
+    speed: "normal",
+    octave: "2"
+};
+const requestScaleAttributes = {
+    key: "E",
+    mode: undefined,
+    speed: "fast",
+    octave: undefined
+};
+const attributes = {
+    defaultScaleAttributes: defaultScaleAttributes,
+    savedScaleAttributes: savedScaleAttributes,
+    requestScaleAttributes: requestScaleAttributes
+};
+const computedScaleAttributes = computeScaleAttributes(attributes);
+const output = remainingScaleAttributes(computedScaleAttributes);
+console.log(output);
+console.log(speechScaleResponse(computedScaleAttributes, attributes.requestScaleAttributes));
+console.log(computeScaleFilename(computedScaleAttributes));
+//# sourceMappingURL=test.js.map
